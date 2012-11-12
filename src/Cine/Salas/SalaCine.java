22a2f425f07f66943189666164eb8f85588e11f4
Edwin Rodriguez <daniel_126@unitec.edu>
2012-11-12 03:17:20 -0600

    Pequeño cambio para la segunda branch
    Recordad trabajar en la branck "TrabajarAqui" para no arruinar el master

---
+++ src/Cine/Salas/SalaCine.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cine.Salas;
+
+/**
+ *
+ * @author Daniel
+ */
+public class SalaCine {
+    
+}
