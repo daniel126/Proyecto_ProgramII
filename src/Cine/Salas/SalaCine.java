8cd11afddffd69d9f56031615155d0ac3207acab
Armando <armandorv14@gmail.com>
2012-11-15 08:27:48 -0600

    probando commit
    hola

--- src/Cine/Salas/SalaCine.java
+++ src/Cine/Salas/SalaCine.java
@@ -9,5 +9,15 @@ package Cine.Salas;
  * @author Daniel
  */
 public class SalaCine {
+    protected int sillas, filas, codigoC;
+    protected double precio;
+    protected boolean activado, ocupado;
     
+    public SalaCine(int sillas, int filas){
+        this.filas=filas;
+        this.sillas=sillas;
+        this.codigoC++;
+        this.ocupado=false;
+        this.activado=true;
+    }
 }
